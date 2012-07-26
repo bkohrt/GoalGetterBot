deaba806d4ed22059b5e2efd1a0248a361da7343
Bengt Kohrt <bengt.kohrt@neuland-bfi.de>
2012-07-26 18:10:42 +0200

    bot script, readme

---
+++ bot.rb
@@ -0,0 +1,20 @@
+require 'net/http'
+require 'json'
+require_relative "./lib/result_calculator"
+
+bot_token = "fwo9yv49o7sw74kjupgi3egv"
+
+url = "http://botliga.de/api/matches/2012"
+response = Net::HTTP.get_response(URI.parse(url))
+matches = JSON.parse(response.body)
+
+http = Net::HTTP.new('botliga.de',80)
+calculator = ResultCalculator.new
+
+matches.each do |match|
+  result = calculator.matchResult match['hostName'], match['guestName']
+  #puts "#{match['hostName']} - #{match['guestName']} #{result[0]}:#{result[1]}"
+  response, data = http.post('/api/guess',"match_id=#{match['id']}&result=#{result[0]}:#{result[1]}&token=#{bot_token}")
+  
+  puts "#{response.code} #{data}" 
+end
